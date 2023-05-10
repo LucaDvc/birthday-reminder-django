c3c27eb0de34d7add36c265bbdeedf6be40cb6a5
luka_dvc <lukadvc3@gmail.com>
2023-05-10 22:32:05 +0300

    changed to sqlite db

--- birthday_reminder/settings.py
+++ birthday_reminder/settings.py
@@ -125,22 +125,22 @@ WSGI_APPLICATION = 'birthday_reminder.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'birthday_reminder',
-        'USER': 'luka',
-        'PASSWORD': 'c85711272',
-        'HOST': 'database-1.czai4obz4r7e.eu-north-1.rds.amazonaws.com',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'birthday_reminder',
+#         'USER': 'luka',
+#         'PASSWORD': 'c85711272',
+#         'HOST': 'database-1.czai4obz4r7e.eu-north-1.rds.amazonaws.com',
+#         'PORT': '5432'
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
