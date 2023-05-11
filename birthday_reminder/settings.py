c75a8eda55456e3008ac427c08d85441eb6099eb
luka_dvc <lukadvc3@gmail.com>
2023-05-11 17:13:09 +0300

    CORS_ORIGIN_ALLOW_ALL = True

--- birthday_reminder/settings.py
+++ birthday_reminder/settings.py
@@ -30,6 +30,8 @@ ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL = 'users.User'
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
